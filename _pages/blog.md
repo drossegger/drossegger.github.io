917d3923e7707dc1c1eba793a8debe9c04ff7b92
Dino Rossegger <dino.rossegger@tutanota.com>
2022-04-04 18:09:50 -0700

    changed blog header

--- _pages/blog.md
+++ _pages/blog.md
@@ -3,6 +3,6 @@ title: Blog
 permalink: /blog/
 layout: posts
 header:
-  overlay_image: /assets/images/blog.jpg
+  overlay_image: /assets/images/blog_overlay.jpg
 classes: wide
 ---
--- jekyllserve.sh
+++ jekyllserve.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-sudo docker run --rm -v ~/workspace_nosync/gitpages/drossegger.github.io/:/srv/jekyll -v ~/workspace_nosync/gitpages/drossegger.github.io/vendor/bundle:/usr/local/bundle -p 4000:4000 -it jekyll/jekyll jekyll serve
+#sudo docker run --rm -v ~/workspace_nosync/gitpages/drossegger.github.io/:/srv/jekyll -v ~/workspace_nosync/gitpages/drossegger.github.io/vendor/bundle:/usr/local/bundle -p 4000:4000 -it jekyll/jekyll jekyll serve
+sudo docker run -v "/home/dino/workspace_nosync/gitpages/drossegger.github.io/:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll /bin/bash -c "jekyll serve --drafts"
+ 
 
