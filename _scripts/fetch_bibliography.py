5c6d492ce3a6827479f3900fd1f134f1f2e48077
Dino Rossegger <dino.rossegger@tutanota.com>
2023-05-02 10:13:19 -0700

    changed directory for bibliography

--- _scripts/fetch_bibliography.py
+++ _scripts/fetch_bibliography.py
@@ -5,18 +5,15 @@ import urllib.request
 zotero_url = 'https://api.zotero.org/groups/1553639/items?format=json&sort=date'
 
 def clean_bib(bib):
-    print(len(bib))
     for x in bib:
         if "creators" not in x["data"].keys():
             bib.remove(x)
-    print(len(bib))
     return bib
 
 
 with urllib.request.urlopen(zotero_url) as url:
     bib=json.loads(url.read().decode())
-    file=open("bibliography.yaml","w")
+    file=open("../_data/bibliography.yml","w")
     yaml.dump(clean_bib(bib),file)
     file.close()
-    print("Bibliography saved")
 
