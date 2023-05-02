3c927b05386fefdee072540a4cdb99d8234babe1
drossegger <drossegger@users.noreply.github.com>
2023-05-02 05:13:46 +0000

    jekyll build from Action 75e1a0e9a4e1c53f11580985ede5852409242f6c

---
+++ assets/js/citations.js
@@ -0,0 +1,138 @@
+function generate_authorsPers(i){
+  var authors='';
+  if (i.data.creators.length != 1) {
+    var j;
+    var authordata = i.data.creators;
+    authors = "with ";
+    for (j = 0; j < authordata.length; j++) {
+      if (!authordata[j].firstName.includes("Dino")) {
+        if (authordata.length > 2) {
+          if (j >= authordata.length - 2 && authordata.length==3) authors = authors + " and ";
+          else if (j >= authordata.length - 2 ) authors = authors + ", and ";
+        }
+        authors = authors + " " + authordata[j].firstName + " " + authordata[j].lastName;
+        if (j < authordata.length - 3) authors = authors + ",";
+      }
+    }
+  }
+  return authors;
+}
+function generate_authors(i){
+   var authors='';
+   if (i.data.creators.length != 0) {
+    var j;
+    var authordata =i.data.creators;
+    authors = "";
+    for (j = 0; j < authordata.length; j++) {
+        if (authordata.length == 2) {
+          if (j>0) authors=authors+ " and";
+        }
+        if (authordata.length > 2) {
+          if (j>0){
+            if (j<authordata.length-1)authors = authors + ",";
+            else authors=authors+", and"
+
+          }
+        }
+        authors = authors + " " + authordata[j].firstName + " " + authordata[j].lastName;
+    }
+  } 
+  return authors;
+
+}
+function processItem(i,personal){
+  item= document.createElement('p');
+  item.className='bibitem';
+  var title = document.createElement("span");
+  title.style.fontStyle = 'italic';
+  title.innerHTML = i.data.title;
+
+  var authors = "";
+  if (personal===undefined || personal==true ) authors=generate_authorsPers(i);
+  else authors=generate_authors(i);
+
+  var secondline = document.createElement("p");
+  var thirdline  = document.createElement("p"); secondline.id="lines";
+  thirdline.id="lines";
+  var authornode = document.createTextNode(authors);
+
+  item.appendChild(title);
+  if (i.data.DOI != "" || i.data.extra.includes("arxiv:") || i.data.url!="" || i.data.extra.includes("arXiv:")) {
+    item.appendChild(document.createTextNode(' ['));
+  }
+  if (i.data.DOI != "") {
+    var doiurl = document.createElement("a");
+    doiurl.href = "https://doi.org/" + i.data.DOI;
+    doiurl.innerHTML = "DOI";
+    item.appendChild(document.createTextNode('\xa0'))
+    item.appendChild(doiurl);
+    if( i.data.extra.toLowerCase().includes("arxiv:")) item.appendChild(document.createTextNode(' |'));
+  }
+  if (i.data.extra.includes("arxiv:")) {
+    var arxiv = document.createElement("a");
+    var arxivlink = i.data.extra.match(/arxiv:([^ ]+).*/)
+    arxiv.href = arxivlink[1];
+    arxiv.innerHTML = "arXiv";
+    item.appendChild(document.createTextNode('\xa0'))
+    item.appendChild(arxiv);
+  }
+  else if (i.data.extra.includes("arXiv:")) {
+    var arxiv = document.createElement("a");
+    var arxivlink = i.data.extra.match(/arXiv:([^ ]+).*/)
+    arxiv.href = "https://arxiv.org/abs/"+arxivlink[1];
+    arxiv.innerHTML = "arXiv";
+    item.appendChild(document.createTextNode('\xa0'))
+    item.appendChild(arxiv);
+
+  }
+  else if (i.data.url!=""){
+    var url=document.createElement("a");
+    url.href=i.data.url;
+    url.innerHTML="pdf";
+    item.appendChild(document.createTextNode('\xa0'))
+    item.appendChild(url);
+  }
+  if (i.data.DOI != "" || i.data.extra.includes("arxiv:") || i.data.url!="" || i.data.extra.includes("arXiv:")) {
+    item.appendChild(document.createTextNode(' ]'));
+  }
+  secondline.appendChild(authornode);
+  if (i.data.itemType == "conferencePaper") {
+    var proceedings=document.createElement("span");
+    proceedings.id='pubtitle';
+    thirdline.appendChild(proceedings);
+    proceedings.innerHTML=i.data.proceedingsTitle;
+    if (i.data.series != '') proceedings.innerHTML=proceedings.innerHTML + ', ' +i.data.series;
+  }
+  if (i.data.itemType == "journalArticle") {
+    var publication=document.createElement("span");
+    publication.id='pubtitle';
+    thirdline.appendChild(publication);
+    publication.innerHTML=i.data.publicationTitle;
+  }
+  if (i.data.volume != "" && i.data.volume!=undefined) {
+    thirdline.appendChild(document.createTextNode(" vol. " + i.data.volume ))
+  }
+  if (i.data.issue !="" && i.data.itemType=="journalArticle" && i.data.issue!=undefined){
+    thirdline.appendChild(document.createTextNode(" (" + i.data.issue+ "),"))
+  }
+  else {thirdline.appendChild(document.createTextNode(","))}
+  thirdline.appendChild(document.createTextNode(" " + i.data.pages))
+  thirdline.appendChild(document.createTextNode(" (" + i.data.date + ")."))
+
+  if ( authors !="" ) {
+    item.appendChild(secondline);
+  }
+  item.appendChild(thirdline);
+  return item;
+}
+
+function createbib(myObj,personal){
+  var bib=document.createElement('div');
+  bib.className="bibliography";
+  for (i = 0; i < myObj.length; i++) {
+    if (myObj[i].data.creators=== undefined) continue;
+    bib.appendChild(processItem(myObj[i],personal));
+  }
+  return bib;
+}
+
---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "New homepage online",
+        "excerpt":"I just created this new homepage with all my professional information. I hope that you find it interesting and useful. I appreciate feedback, just write me an e-mail.  ","categories": ["news"],
+        "tags": [],
+        "url": "https://drossegger.github.io/pages/drossegger/news/homepage-online/",
+        "teaser": null
+      },{
+        "title": "New preprint on arXiv!",
+        "excerpt":"My newest preprint Degree spectra of analytic complete equivalence relations just appeared on arXiv. This article investigates the bi-embeddability and elementary bi-embeddability relation on countable structures both from the viewpoint of descriptive set theory and computable structure theory. Two structures are bi-embeddable if either is isomorphic to an substructure of...","categories": ["news"],
+        "tags": [],
+        "url": "https://drossegger.github.io/pages/drossegger/news/spectra-analyticcomp/",
+        "teaser": null
+      },{
+        "title": "Positive enumerable functors",
+        "excerpt":"We just submitted a new article titled “Positive enumerable functors” and you can find the preprint here or on arxiv. This article is joint work with Barbara Csima and Daniel Yu who proved most of the results presented there in an undergraduate research project he did with Barbara and me...","categories": ["news"],
+        "tags": [],
+        "url": "https://drossegger.github.io/pages/drossegger/news/positiveenumerablefunctors/",
+        "teaser": null
+      },{
+        "title": "Turing degrees that compute HYP but can not compute Kleene's _O_.",
+        "excerpt":"Let \\(HYP\\) be the set of hyperarithmetic degrees, i.e. \\(HYP=\\{ \\mathbf d:\\exists (\\alpha&lt;\\omega_1^{\\mathrm{CK}}) \\mathbf d&lt; \\mathbf 0^{(\\alpha)}\\}.\\) Jockusch and Simpson [1] showed that there is a degree \\(\\mathbf d\\) that is minimal over \\(HYP\\) (i.e., if \\(\\mathbf c&lt;\\mathbf d\\), then \\(\\mathbf c\\in HYP\\)) and such that \\(\\mathbf d^{(3)}=deg(\\mathcal O)\\) where...","categories": [],
+        "tags": [],
+        "url": "https://drossegger.github.io/pages/drossegger/degreescomputinghyp/",
+        "teaser": null
+      },{
+        "title": "The degrees of categoricity above 0''",
+        "excerpt":"Consider the ordering \\(\\omega\\) of the natural numbers \\(0\\leq 1\\leq 2\\leq 3\\leq\\dots\\). We can build an isomorphic order \\(\\mathcal B\\) (for bad) as follows: We order all the even numbers in ascending order and enumerate the Halting set. If \\(n\\) enters the Halting set at some stage \\(s\\), then we...","categories": [],
+        "tags": [],
+        "url": "https://drossegger.github.io/pages/drossegger/degreesofcat/",
+        "teaser": null
+      }]
